056adcec845502392e2f8cd01faa699cafbb7438
wh0am123 <thejyjco@gmail.com>
2018-08-02 13:36:58 -0430

    Juan here

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var multer = require('multer');
 var upload = multer();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var mongoose = require('mongoose');
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
 /* GET home page. */
@@ -16,8 +17,43 @@ router.post('/login', function(req, res){
    res.send("received your request!");
 });
 router.post('/signup', function(req, res){
-   console.log(req.body);
-   res.send("received your request!");
+   var user = req.body;
+   if(!user.name || !user.email || !user.password) {
+   		res.render('signup', {
+   			status: 'formError'
+   		});
+   } else {
+
+   		mongoose.connect('mongodb://localhost/shogun');
+
+   		var userSchema = mongoose.Schema({
+		name: String,
+		email: String,
+		password: String
+		});
+
+		var User = mongoose.model('User', userSchema);
+
+   		var newUser = new User({
+   			name: user.name,
+   			email: user.email,
+   			password: user.password
+   		});
+   		newUser.save((err, User) => {
+   			if(err) {
+   				res.render('signup', {
+   					status: 'error',
+   					details: err
+   				});
+   			} else {
+   				/*res.render('dashboard', {
+   					status: 'new',
+   					user: user
+   				});*/
+   				res.send('All OK');
+   			}
+   		});
+   }
 });
 
 module.exports = router;
